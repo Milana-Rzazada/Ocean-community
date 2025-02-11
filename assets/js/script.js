eebc2f2f63c206db553dbf93de4850c0d9ccd531
Milana-Rzazade <71l1c0h@code.edu.az>
2025-02-10 23:16:15 -0500

    new code 10 february

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,16 @@ icon.addEventListener('click', () => {
   menu.classList.toggle('active');
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+  const dropdowns = document.querySelectorAll(".dropdown");
+
+  dropdowns.forEach(dropdown => {
+      dropdown.addEventListener("mouseenter", function() {
+          this.querySelector(".dropdown-menu").style.display = "block";
+      });
+
+      dropdown.addEventListener("mouseleave", function() {
+          this.querySelector(".dropdown-menu").style.display = "none";
+      });
+  });
+});
